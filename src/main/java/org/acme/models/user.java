8e32ea1d28dc1a51258cd8b45ae8a6ba27460b93
AdithiKashain <94888785+AdithiKashain@users.noreply.github.com>
2025-02-07 13:42:39 +0530

    Update user.java

--- src/main/java/org/acme/models/user.java
+++ src/main/java/org/acme/models/user.java
@@ -1,3 +1,31 @@
+package org.acme.models;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class user {
-    
+
+    public String user_id;
+
+    @Column(nullable = false)
+    public String username;
+
+    @Column(nullable = false)
+    public String password;
+
+	public List<String> message_ids;
+
+
+    public user() {}
+
+    public user(String user_id, String username, String password, List<String> message_ids) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.message_ids = message_ids;
+    }
+
+	public void add_message(String message_id) {
+		this.message_ids.add(message_id);
+	}
 }
